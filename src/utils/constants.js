7ee3cab7010551f4751f2bd14bfa288cd22cb797
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-01 10:22:51 +0530

    TMDB api call for now-playing-movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,10 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-  
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTc2YzlhNDhjYTYxZWJmYmI5ODVhMTA2MTdjNWU3MCIsInN1YiI6IjY1OTFhNTRlZTY0MGQ2N2VlMGQ1ZTdkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cqoJDGia8eOcyXveiqkeTqmdtV3BrfimZHORpW1TY2k'
+    }
+  };  
