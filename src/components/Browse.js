a7b1cf20ecb7afeeefb4872e28e433dace58a65e
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-02 15:44:10 +0530

    built MainContainer

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer';
 
 
 const Browse = () => {
@@ -9,6 +10,7 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
       {/* 
           MainContainer
             - VideoBackground
