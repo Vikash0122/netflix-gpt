0618d61c68529224f4a967d4c8e42d574dbba832
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-04 13:29:50 +0530

    Hard coded string to constants

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import GptSearchBar from './GptSearchBar'
 import GptMovieSuggestions from './GptMovieSuggestions'
+import { BG_URL } from '../utils/constants'
 
 const GptSearch = () => {
   return (
     <div>
       <div className="absolute -z-10">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          src={BG_URL}
           alt="back-image"
         />
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,3 +20,5 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "italian", name: "Italian"},
   { identifier: "spanish", name: "Spanish" },
 ];
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
