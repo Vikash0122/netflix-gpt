44bb5b5aff2f9ec82fbcd2cdb2a33c321b9d136c
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-04 23:51:47 +0530

    openai GPT search done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTc2YzlhNDhjYTYxZWJmYmI5ODVhMTA2MTdjNWU3MCIsInN1YiI6IjY1OTFhNTRlZTY0MGQ2N2VlMGQ1ZTdkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cqoJDGia8eOcyXveiqkeTqmdtV3BrfimZHORpW1TY2k'
+      Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY,
     }
   }; 
   
@@ -22,3 +22,6 @@ export const SUPPORTED_LANGUAGES = [
 ];
 
 export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
